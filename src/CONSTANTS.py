893f079a0d7e30a8799b270ea5d2dcd3f3ecb58c
Luke Alie <luke.d.alie@gmail.com>
2024-04-30 16:54:06 -0400

    This is a clean start. Looking to rebuild the program with a clean file structure and further incorporated feedback.

---
+++ src/CONSTANTS.py
@@ -0,0 +1,5 @@
+ALL_INGREDIENTS = ["Mushroom","Sprout","Toad","Talon","Orchid","Root","Scorpion","Feather"]
+
+# values indicate positive or negative. ordered as [r , g, b]
+ALL_ALCHEMICALS = [[-1, 1, -1],[1, -1, 1],[1, -1, -1],[-1, 1, 1],[-1, -1, 1],[1, 1, -1],[-1, -1, -1],[1, 1, 1]]
+
---
+++ src/main.py
@@ -0,0 +1,34 @@
+'''
+Create a list of all permutations of alchemicals.
+
+Enter while loop that stops when all alchemicals are determined.
+
+Take in a fact from a user. 
+
+Check each permutation against each fact, and return a new list with all possible permutations. 
+
+Display those possible permutations in a grid resembling the player board. 
+
+'''
+from CONSTANTS import ALL_INGREDIENTS,ALL_ALCHEMICALS
+from buildAllAssignmentSets import buildAllAssignmentSets
+
+def main():
+    Facts = []
+    # take in the list of ALL_ALCHEMICALS. return a list of dictionaries pairing each assignment set with a multiplier of 1.
+    possibleAssignmentSets = buildAllAssignmentSets(ALL_ALCHEMICALS,ALL_INGREDIENTS)
+    # initiate loop that will end once the entire board is known.
+    while boardNotComplete:
+        # prompt user for new fact. add that fact to Facts list. 
+        getFactFromUser(Facts,ALL_ALCHEMICALS,ALL_INGREDIENTS)
+        # check each alchemical permutation in the list of possiblePermutations against each fact. filter out impossible permutations.
+        factCheckPermutations(Facts,possiblePermutations)
+        # take in the list of possible permutations. return list of dictionaries containing the probability for each ingredient-alchemical assignemnt.
+        assignmentProbabilities = calculateAssignmentProbabilities(ALL_INGREDIENTS,possiblePermutations)
+        # display the probability of each ingredient-alchemical assignment as grid to user.
+        displayProbabilities(assignmentProbabilities)
+        # check if the all ingredient-achemical assignments are known.
+        boardNotComplete = checkForCompletedBoard(assignmentProbailities)
+
+
+
